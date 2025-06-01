654e06d5ebfdd9d6ba1924b7530fbde112208c29
ELIS0407 <elisabethtrijulianakandakon@gmail.com>
2025-06-02 07:11:24 +0800

    Perbaikan tampilan halaman About

---
+++ resources/views/about.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>Daftar Guru - LangGo!</title>
+  <style>
+    body {
+      margin: 0;
+      font-family: Arial, sans-serif;
+      display: flex;
+      height: 100vh;
+    }
+    .left {
+      background-color: #4a7392;
+      width: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .right {
+      width: 50%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding: 0 40px;
+    }
+    input {
+      margin-bottom: 15px;
+      padding: 10px;
+      width: 100%;
+      border-radius: 8px;
+      border: 1px solid #ccc;
+    }
+    button {
+      background-color: #4a7392;
+      color: white;
+      padding: 10px;
+      border: none;
+      border-radius: 8px;
+      cursor: pointer;
+    }
+  </style>
+</head>
+<body>
+  <div class="left">
+    <img src="https://i.pinimg.com/736x/c0/05/26/c00526d23e9102ce99532950d35b7b34.jpg" width="200" alt="Logo LangGo">
+  </div>
+  <div class="right">
+    <h2>LangGo! Belajar Bahasa Cepat dan Seru Berbasis WEB</h2>
+    <form method="post" action="proses_daftar_guru.php">
+      <input type="text" name="username" placeholder="Username.." required>
+      <input type="email" name="email" placeholder="Email.." required>
+      <input type="text" name="telepon" placeholder="No. telepon.." required>
+      <input type="password" name="password" placeholder="Password.." required>
+      <button type="submit">Sign Up</button>
+    </form>
+  </div>
+</body>
+</html
